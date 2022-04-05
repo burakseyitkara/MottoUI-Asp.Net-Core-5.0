98d3c871ed31e69fcc1f65927a23cf9efe04c05b
Burak Seyit <burakseyitkara@icloud.com>
2022-04-06 02:18:22 +0300

    #37 Writer Blog relation

--- MottoUI.BusinessLayer/Concrete/BlogManager.cs
+++ MottoUI.BusinessLayer/Concrete/BlogManager.cs
@@ -53,5 +53,10 @@ namespace MottoUI.BusinessLayer.Concrete
         {
             return _blogDal.GetListAll();
         }
+
+        public List<Blog> GetBlogListByWriter(int id)
+        {
+            return _blogDal.GetListAll(x => x.WriterID == id);
+        }
     }
 }
---
+++ MottoUI/ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MottoUI.BusinessLayer.Concrete;
+using MottoUI.DataAccessLayer.EntityFramework;
+
+namespace MottoUI.ViewComponents.Blog
+{
+    public class WriterLastBlog:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListByWriter(2);
+            return View(values);
+        }
+    }
+}
