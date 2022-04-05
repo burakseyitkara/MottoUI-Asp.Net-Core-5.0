184b6c3b644d94fe216b6e0a20fc5e3fa3a4f919
Burak Seyit <burakseyitkara@icloud.com>
2022-04-05 10:30:13 +0300

    #30 ViewComponent

---
+++ MottoUI/ViewComponents/CommentList.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MottoUI.Models;
+using System.Collections.Generic;
+
+namespace MottoUI.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment{
+                    ID=1,
+                    Username="Burak",
+                },
+                new UserComment{
+                    ID=2,
+                    Username="Elif",
+                },
+                new UserComment{
+                    ID=3,
+                    Username="Kemal",
+                },
+
+            };
+            return View(commentvalues);
+        }
+
+    }
+}
