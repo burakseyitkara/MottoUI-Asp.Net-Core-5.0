ff93dd13b7f39023d79a3b7803d4873fe539d0e7
Burak Seyit <burakseyitkara@icloud.com>
2022-04-05 11:51:38 +0300

    #33 Register Page

---
+++ MottoUI.BusinessLayer/Concrete/WriterManager.cs
@@ -0,0 +1,26 @@
+﻿using MottoUI.BusinessLayer.Abstract;
+using MottoUI.DataAccessLayer;
+using MottoUI.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MottoUI.BusinessLayer.Concrete
+{
+    public class WriterManager: IWriterService
+    {
+        IWriteDal _writerdal;
+
+        public WriterManager(IWriteDal writerdal)
+        {
+            _writerdal = writerdal;
+        }
+
+        public void WriterAdd(Writer writer)
+        {
+            _writerdal.Insert(writer);
+        }
+    }
+}
