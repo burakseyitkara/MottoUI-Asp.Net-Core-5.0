f5c2deb6c58e480ff2ce3cebceacacf3e53a151b
Burak Seyit <burakseyitkara@icloud.com>
2022-04-08 13:20:27 +0300

    #41 About

---
+++ MottoUI.BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,26 @@
+﻿using MottoUI.BusinessLayer.Abstract;
+using MottoUI.DataAccessLayer.Abstract;
+using MottoUI.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MottoUI.BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public List<About> GetList()
+        {
+            return _aboutDal.GetListAll();
+        }
+    }
+}
---
+++ MottoUI/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MottoUI.BusinessLayer.Concrete;
+using MottoUI.DataAccessLayer.EntityFramework;
+
+namespace MottoUI.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var values = abm.GetList();
+            
+            return View(values);
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
+}
