d61ac752398854c9bfd5a5be9e88ab82ac4f07ce
Burak Seyit <burakseyitkara@icloud.com>
2022-04-08 15:17:58 +0300

    #47 Return Login Url with Authorize

--- MottoUI/Startup.cs
+++ MottoUI/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,13 +28,24 @@ namespace MottoUI
         {
             services.AddControllersWithViews();
             services.AddSession();
-            services.AddMvc(config=>
+            services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder()
                 .RequireAuthenticatedUser()
                 .Build();
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
+
+            services.AddMvc();
+            services.AddAuthentication(
+
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/Index";
+
+                }
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
