8014405be475774236c1b6aa5b9798330031726b
Burak Seyit <burakseyitkara@icloud.com>
2022-04-11 12:41:53 +0300

    #51 Writer Navbar

--- MottoUI/Controllers/WriterController.cs
+++ MottoUI/Controllers/WriterController.cs
@@ -23,5 +23,16 @@ namespace MottoUI.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
     }
 }
