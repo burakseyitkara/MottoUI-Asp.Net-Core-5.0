2989dd8be32dfdee450e7e67883007e0cdcdd136
Burak Seyit <burakseyitkara@icloud.com>
2022-04-11 12:12:58 +0300

    #50 Writer Panel Theme

--- MottoUI/Controllers/WriterController.cs
+++ MottoUI/Controllers/WriterController.cs
@@ -18,5 +18,10 @@ namespace MottoUI.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
