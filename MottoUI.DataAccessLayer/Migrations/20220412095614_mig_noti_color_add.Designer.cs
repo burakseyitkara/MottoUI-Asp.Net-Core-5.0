2d191748fb847cb264fcee6503808712f4acf486
Burak Seyit <burakseyitkara@icloud.com>
2022-04-12 13:16:20 +0300

    #76 Notification stuff

--- MottoUI.DataAccessLayer/Migrations/20220412095614_mig_noti_color_add.Designer.cs
+++ MottoUI.DataAccessLayer/Migrations/20220412095614_mig_noti_color_add.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MottoUI.DataAccessLayer.Concrete;
 
 namespace MottoUI.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220412095614_mig_noti_color_add")]
+    partial class mig_noti_color_add
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MottoUI/ViewComponents/Writer/WriterNotification.cs
+++ MottoUI/ViewComponents/Writer/WriterNotification.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MottoUI.BusinessLayer.Concrete;
+using MottoUI.DataAccessLayer.EntityFramework;
 
 namespace MottoUI.ViewComponents.Writer
 {
     public class WriterNotification:ViewComponent
     {
+        NotificationManager nm = new NotificationManager(new EfNotificationRepository());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = nm.GetList();
+            return View(values);
         }
     }
 }
