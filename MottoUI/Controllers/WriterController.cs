2eec01607b2aeb4b3d0f548a9ac41a209d128b85
Burak Seyit <burakseyitkara@icloud.com>
2022-04-12 11:54:29 +0300

    #72 Writer Edit Profile

--- MottoUI/Controllers/WriterController.cs
+++ MottoUI/Controllers/WriterController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MottoUI.BusinessLayer.Concrete;
+using MottoUI.BusinessLayer.ValidationRules;
+using MottoUI.DataAccessLayer.EntityFramework;
+using MottoUI.EntityLayer.Concrete;
 
 namespace MottoUI.Controllers
 {
     public class WriterController : Controller
     {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
         public IActionResult Index()
         {
             return View();
@@ -33,6 +39,33 @@ namespace MottoUI.Controllers
         public PartialViewResult WriterFooterPartial()
         {
             return PartialView();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            var writervalues = wm.TGetById(1);
+            return View(writervalues);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p)
+        {
+            WriterValidator wl = new WriterValidator();
+            ValidationResult results = wl.Validate(p);
+            if (results.IsValid)
+            {
+                wm.TUpdate(p);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
