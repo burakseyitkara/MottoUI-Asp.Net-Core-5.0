01743c47e553394c3f41ed44684aa88cfeb7f718
Burak Seyit <burakseyitkara@icloud.com>
2022-04-12 14:44:50 +0300

    #79 All Notification

--- MottoUI/Controllers/NotificationController.cs
+++ MottoUI/Controllers/NotificationController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MottoUI.BusinessLayer.Concrete;
+using MottoUI.DataAccessLayer.EntityFramework;
 
 namespace MottoUI.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager nm = new NotificationManager(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AllNotifications()
+        {
+            var values = nm.GetList();
+            return View(values);
         }
     }
 }
