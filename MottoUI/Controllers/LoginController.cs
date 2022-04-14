3bda280a8687fc71f01f25de72a092011d536f26
Burak Seyit <burakseyitkara@icloud.com>
2022-04-15 00:50:00 +0300

    #125 login with identity

--- MottoUI/Controllers/LoginController.cs
+++ MottoUI/Controllers/LoginController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MottoUI.DataAccessLayer.Concrete;
 using MottoUI.EntityLayer.Concrete;
+using MottoUI.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -11,36 +13,60 @@ using System.Threading.Tasks;
 
 namespace MottoUI.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
-        [AllowAnonymous]
+
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Index(Writer p)
+        public async Task<IActionResult> Index(UserSignInViewModel p)
         {
-            Context c = new Context();
-            var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == x.WriterMail && x.WriterPassword == p.WriterPassword);
-            if (datavalue != null)
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim>
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+                if (result.Succeeded)
                 {
-                new Claim(ClaimTypes.Name,p.WriterMail)
-                };
-                var userIdentity = new ClaimsIdentity(claims, "login");
-                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-                await HttpContext.SignInAsync(principal);
-                return RedirectToAction("Index", "Dashboard");
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Login");
+
+                }
             }
-            else
-            {
-                ViewBag.Error = "Kullanıcı adı veya şifre hatalı";
-                return View();
-            }
+            return View();
         }
+        //[HttpPost]
+        //public async Task<IActionResult> Index(Writer p)
+        //{
+        //    Context c = new Context();
+        //    var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == x.WriterMail && x.WriterPassword == p.WriterPassword);
+        //    if (datavalue != null)
+        //    {
+        //        var claims = new List<Claim>
+        //        {
+        //        new Claim(ClaimTypes.Name,p.WriterMail)
+        //        };
+        //        var userIdentity = new ClaimsIdentity(claims, "login");
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+        //        await HttpContext.SignInAsync(principal);
+        //        return RedirectToAction("Index", "Dashboard");
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Error = "Kullanıcı adı veya şifre hatalı";
+        //        return View();
+        //    }
+        //}
     }
 }
 
---
+++ MottoUI/Models/UserSignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MottoUI.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz.")]
+        public string username { get; set; }
+         
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        public string password { get; set; }
+    }
+}
