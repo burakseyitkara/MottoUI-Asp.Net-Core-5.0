5f15e6235c4ede3ce45c93a6e04dfd9ee42b40d2
Burak Seyit <burakseyitkara@icloud.com>
2022-04-14 10:45:03 +0300

    #103 OpenWeather Api

--- MottoUI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ MottoUI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -3,6 +3,7 @@ using MottoUI.BusinessLayer.Concrete;
 using MottoUI.DataAccessLayer.Concrete;
 using MottoUI.DataAccessLayer.EntityFramework;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace MottoUI.Areas.Admin.ViewComponents.Statistic
 {
@@ -15,6 +16,13 @@ namespace MottoUI.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            string api = "52bd5319d2df4270db71d10a7a26f159";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=konya&mode=xml&lang=tr&units=metric&appid=" + api;
+
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
             return View();
         }
     }
