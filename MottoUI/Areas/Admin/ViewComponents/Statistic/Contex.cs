1f3a0c4b06808ff3a1a189520a31a591c632852e
Burak Seyit <burakseyitkara@icloud.com>
2022-04-14 09:53:25 +0300

    #101 Adding Admin Table to database

---
+++ MottoUI/Areas/Admin/ViewComponents/Statistic/Contex.cs
@@ -0,0 +1,6 @@
+﻿namespace MottoUI.Areas.Admin.ViewComponents.Statistic
+{
+    internal class Contex
+    {
+    }
+}
---
+++ MottoUI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MottoUI.BusinessLayer.Concrete;
+using MottoUI.DataAccessLayer.Concrete;
+using MottoUI.DataAccessLayer.EntityFramework;
+using System.Linq;
+
+namespace MottoUI.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.GetList().Count();
+            ViewBag.v2 = c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
+            return View();
+        }
+    }
+}
