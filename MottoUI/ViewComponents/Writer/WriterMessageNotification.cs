e869985e2116a0d48122eb779533c142ae217b15
Burak Seyit <burakseyitkara@icloud.com>
2022-04-15 15:12:53 +0300

    #136 admin panel message

--- MottoUI/ViewComponents/Writer/WriterMessageNotification.cs
+++ MottoUI/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MottoUI.BusinessLayer.Concrete;
+using MottoUI.DataAccessLayer.Concrete;
 using MottoUI.DataAccessLayer.EntityFramework;
+using System.Linq;
 
 namespace MottoUI.ViewComponents.Writer
 {
     public class WriterMessageNotification:ViewComponent
     {
         Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
-            int id = 2;
-            var values = mm.GetInboxListByWriter(id);
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(x => x.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(x => x.WriterID).FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerID);
             return View(values);
         }
         
