4036e9d2eba257f9ff1204380d969191b434de0c
Burak Seyit <burakseyitkara@icloud.com>
2022-04-15 10:35:40 +0300

    #126 login with identity, get user veri

--- MottoUI/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ MottoUI/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -9,11 +9,13 @@ namespace MottoUI.ViewComponents.Writer
     public class WriterAboutOnDashboard:ViewComponent
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
+        Context c = new Context();
 
         public IViewComponentResult Invoke()
         {
-            var usermail = User.Identity.Name;
-            Context c = new Context();
+            var username = User.Identity.Name;
+            ViewBag.veri = username;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(x => x.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(x => x.WriterID).FirstOrDefault();
             var values = wm.GetWriterById(writerID);
             return View(values);
