c8118d9f1c5486ac5edcd8ccd185461a4a270db6
Burak Seyit <burakseyitkara@icloud.com>
2022-04-17 12:25:39 +0300

    #136

--- MottoUI/Areas/Admin/Controllers/AdminMessageController.cs
+++ MottoUI/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MottoUI.BusinessLayer.Concrete;
+using MottoUI.DataAccessLayer.Concrete;
+using MottoUI.DataAccessLayer.EntityFramework;
+using System.Linq;
 
 namespace MottoUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminMessageController : Controller
     {
+        Context c = new Context();
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
         public IActionResult Inbox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(x => x.WriterID).FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerID);
+            return View(values);
+        }
+
+        public IActionResult SendBox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(x => x.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(x => x.WriterID).FirstOrDefault();
+
+            var values = mm.GetSendBoxListByWriter(writerID);
+            return View(values);
+        }
+        public IActionResult ComposeMessage()
         {
             return View();
         }
